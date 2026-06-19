ed5546dfe5eefc115d26318a40e56f67cf752d1d
VerkhivskyiMykola <mykola.verkhivskyi@gmail.com>
2026-06-19 16:28:01 +0200

    Add "/health"
    Check status of database and ollama

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 import os
 import requests
+import psycopg2
 
 app = FastAPI()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 OLLAMA_URL = os.getenv("OLLAMA_URL")
 
-@app.get("/")
-def root():
-    return {"status": "backend is running"}
-
-@app.get("/ollama")
-def test_ollama():
-    r = requests.get(f"{OLLAMA_URL}/api/tags")
-    return r.json()
+@app.get("/health")
+def health():
+    db_ok = False
+    ollama_ok = False
+    try:
+        r = requests.get(f"{OLLAMA_URL}/api/tags", timeout=2)
+        ollama_ok = r.status_code == 200
+    except:
+        ollama_ok = False
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        conn.close()
+        db_ok = True
+    except:
+        db_ok = False
+    return {
+        "status": "ok",
+        "db": db_ok,
+        "ollama": ollama_ok
+    }
